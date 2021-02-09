d3a747c963a1ab9f311235c6d04ac1cd435c8891
Volkov_Ivan <volkov_is@ssk-gaz.ru>
2021-02-09 23:41:58 +0300

    search and download excel file

--- manually.py
+++ manually.py
@@ -1,9 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 
-html = requests.request('GET', 'https://guu.ru/%d1%81%d1%82%d1%83%d0%b4%d0%b5%d0%bd%d1%82%d0%b0%d0%bc/%d1%80%d0%b0%d1%81%d0%bf%d0%b8%d1%81%d0%b0%d0%bd%d0%b8%d0%b5-%d1%81%d0%b5%d1%81%d1%81%d0%b8%d0%b9/schedule').text
-bs_obj = BeautifulSoup(html, 'html.parser')
+
+html = requests.request(
+    'GET', 'https://guu.ru/%d1%81%d1%82%d1%83%d0%b4%d0%b5%d0%bd%d1%82%d0%b0%d0%bc/%d1%80%d0%b0%d1%81%d0%bf%d0%b8%d1%81%d0%b0%d0%bd%d0%b8%d0%b5-%d1%81%d0%b5%d1%81%d1%81%d0%b8%d0%b9/schedule'
+)
+bs_obj = BeautifulSoup(html.text, 'html.parser')
+base_url = 'https://guu.ru'
 
 for i in bs_obj.findAll('span', attrs={'class': 'doc-unit-title'}):
     if 'ОЗФО' in i.get_text():
-        print(i)
+        relative_path = i.parent.attrs['href']
+        file_name = relative_path.split('/')[-1]
+        html_file = requests.request('GET', base_url + relative_path)
+        with open(file_name, 'wb') as file:
+            file.write(html_file.content)
+        break
