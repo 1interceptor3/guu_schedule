a0e5e73aac741769f184f666c7ab7beee9818f11
Volkov Ivan <volkov_is@ssk-gaz.ru>
2021-02-12 16:36:21 +0300

    start working with worksheet, institute and ed. programs

--- db.py
+++ db.py
@@ -4,6 +4,9 @@ import os
 
 
 class DataBaseSQLITE:
+    """
+    Класс для работы с базой данных
+    """
     def __init__(self):
         print('Start working with db')
         if os.path.exists('guu.sqlite3'):
@@ -39,33 +42,52 @@ class DataBaseSQLITE:
     def last_changes(self):
         self.cursor.execute('SELECT * FROM downloaded_file WHERE id = (SELECT MAX(id) from downloaded_file);')
         last = self.cursor.fetchone()
-        return last[-1]
+        if last:
+            return last[-1]
+        else:
+            return last
 
     def need_to_update(self):
         last_date = self.last_changes()
-        print(last_date)
-        date_in_db = datetime.date(int(last_date.split('-')[0]), int(last_date.split('-')[1]), int(last_date.split('-')[2]))
-        print(date_in_db)
+        date_in_db = datetime.date(
+            int(last_date.split('-')[0]), int(last_date.split('-')[1]), int(last_date.split('-')[2])
+        )
         delta = datetime.date.today() - date_in_db
         if delta >= datetime.timedelta(days=1):
-            print('Разница 1 день или более')
+            print('Разница => 1 день')
             return True
         else:
-            print('Разница менее 1 дня')
+            print('Ранзница < 1 день')
             return False
+
+    def update_years(self, years_list: list):
+        if type(years_list) == list:
+            # Чистим прошлые значения
+            self.cursor.execute('DELETE FROM year;')
+            self.conn.commit()
+
+            # Проверяем листы и добавим в БД
+            output = list()
+            for i in years_list:
+                if 'ОЗФО' in i:
+                    self.cursor.execute(f"INSERT INTO year (number) VALUES ('{i}');")
+                    output.append(i)
+            self.conn.commit()
+            print('Years updated')
+            return output
+
+    def get_years(self):
+        self.cursor.execute("SELECT number FROM year;")
+        output = []
+        for i in self.cursor.fetchall():
+            for j in i:
+                output.append(j)
+        return output
+
+    def update_institute(self):
+        return None
 
     def close_conn(self):
         self.cursor.close()
         self.conn.close()
         print('Connection closed')
-
-
-db_obj = DataBaseSQLITE()
-
-# if db_obj.need_to_update():
-#     print('Пора обновлять')
-# else:
-#     print('Еще можно подождать')
-db_obj.need_to_update()
-
-db_obj.close_conn()
--- manually.py
+++ manually.py
@@ -1,23 +1,9 @@
 from bs4 import BeautifulSoup
-from openpyxl import load_workbook
+import openpyxl
+from openpyxl.utils import get_column_letter, column_index_from_string
 import requests
+import os
 from db import DataBaseSQLITE
-
-
-# html = requests.request(
-#     'GET', 'https://guu.ru/%d1%81%d1%82%d1%83%d0%b4%d0%b5%d0%bd%d1%82%d0%b0%d0%bc/%d1%80%d0%b0%d1%81%d0%bf%d0%b8%d1%81%d0%b0%d0%bd%d0%b8%d0%b5-%d1%81%d0%b5%d1%81%d1%81%d0%b8%d0%b9/schedule'
-# )
-# bs_obj = BeautifulSoup(html.text, 'html.parser')
-# base_url = 'https://guu.ru'
-#
-# for i in bs_obj.findAll('span', attrs={'class': 'doc-unit-title'}):
-#     if 'ОЗФО' in i.get_text():
-#         relative_path = i.parent.attrs['href']
-#         file_name = relative_path.split('/')[-1]
-#         html_file = requests.request('GET', base_url + relative_path)
-#         with open(file_name, 'wb') as file:
-#             file.write(html_file.content)
-#         break
 
 
 def make_obj(url):
@@ -26,13 +12,35 @@ def make_obj(url):
     return bs_obj
 
 
+def get_excel_file():
+    for i in os.listdir():
+        if '.xlsx' in i:
+            return i
+    return None
+
+
 class Guu:
     link_schedule = 'https://guu.ru/%d1%81%d1%82%d1%83%d0%b4%d0%b5%d0%bd%d1%82%d0%b0%d0%bc/%d1%80%d0%b0%d1%81%d0%bf' \
                     '%d0%b8%d1%81%d0%b0%d0%bd%d0%b8%d0%b5-%d1%81%d0%b5%d1%81%d1%81%d0%b8%d0%b9/schedule'
     base_url = 'https://guu.ru'
 
-    # def __init__(self):
-    #     self.db_obj = DataBaseSQLITE()
+    def __init__(self):
+        self.db_obj = DataBaseSQLITE()
+
+        # Проверка на необходимость обновления файла и на наличие файла
+        if self.db_obj.need_to_update() or not get_excel_file():
+            # Если нужно обновление или файла нету, проходим по каталогу и удаляем лишние файлы excel
+            for i in os.listdir():
+                if '.xlsx' in i:
+                    os.remove(i)
+            # Получаем новое название скачанного файла
+            self.excel_file_name = self.download_file()
+            print('New file', self.excel_file_name)
+            self.work_with_excel(new=True, file_name=self.excel_file_name)
+        else:
+            self.excel_file_name = get_excel_file()
+            print('Old file', self.excel_file_name)
+            self.work_with_excel(new=False, file_name=self.excel_file_name)
 
     def download_file(self):
         obj = make_obj(self.link_schedule)
@@ -45,12 +53,47 @@ class Guu:
                     file.write(html_file.content)
                 return file_name
 
-    def load_excel(self, file_name=None):
-        wb = load_workbook(file_name)
-        return wb.sheetnames
+    def work_with_excel(self, new: bool, file_name=None):
+        wb = openpyxl.load_workbook(filename=file_name, read_only=True)
+        if new:
+            sheets = self.db_obj.update_years(wb.sheetnames)
+        else:
+            sheets = self.db_obj.get_years()
+
+        # Перебираем листы и выбираем институты
+        for sheet in sheets:
+            ws = wb[sheet]
+            institute_coordinate, program_coordinate, institute_list, program_list = None, None, list(), list()
+
+            for row in ws.rows:
+                for cell in row:
+                    if cell.value == 'ИНСТИТУТ':
+                        institute_coordinate = cell.coordinate
+                        continue
+                    if cell.value == 'ОБРАЗОВАТЕЛЬНАЯ ПРОГРАММА':
+                        program_coordinate = cell.coordinate
+                        continue
+            print('Найденные координаты', institute_coordinate, program_coordinate)
+            print('--------------------')
+            # Перебор строки институтов
+            for i in range(column_index_from_string(institute_coordinate[0]) + 1, ws.max_column + 1):
+                if ws.cell(row=int(institute_coordinate[1]), column=i).value:
+                    institute_list.append(ws.cell(row=int(institute_coordinate[1]), column=i).value)
+
+            # Перебор строки программ обучения
+            # Не видит объединенные ячейки, ИСПРАВИТЬ
+            for i in range(column_index_from_string(program_coordinate[0]) + 1, ws.max_column + 1):
+                if ws.cell(row=int(program_coordinate[1]), column=i).value:
+                    program_list.append(ws.cell(row=int(program_coordinate[1]), column=i).value)
+
+            print(ws.cell(row=6, column=10).value)
+            print('--------------------')
+            break  # Проверяем пока только 1 лист
+
+        wb.close()
+
+    def __del__(self):
+        self.db_obj.close_conn()
 
 
 guu_obj = Guu()
-excel = guu_obj.download_file()
-print(excel)
-print(guu_obj.load_excel(excel))
